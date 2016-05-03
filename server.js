f1daac6d54f5affc569905a9111ed456469f4e6e
Lukas Ruge <lukeslog@googlemail.com>
2016-05-03 23:44:19 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var bonjour = require('bonjour')();
+
+bonjour.find({
+    type: 'http'
+  }, function(service) {
+    console.log(JSON.stringify(service.host));
+    if (service.name == 'IceBox') {
+      app.get('/serviceip', function(req, res) {
+        res.json({ ip: service.host })
+      });
+      app.listen(80);
+    }
+  });
+
+  app.get('/', function(req, res) {
+    app.use(express.static('./image'));
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
