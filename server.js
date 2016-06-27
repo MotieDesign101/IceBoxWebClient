69ee3b7662995e2e8ab9b6989d4c666990e6bbcc
Lukas Ruge <lukeslog@googlemail.com>
2016-06-27 18:42:02 +0200

    adding reset page

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var bonjour = require('bonjour')();
+var exec = require('exec');
 
 bonjour.find({
     type: 'http'
@@ -9,13 +10,26 @@ bonjour.find({
     console.log(JSON.stringify(service.host));
     if (service.name == 'IceBox') {
       app.get('/serviceip', function(req, res) {
-        res.json({ ip: service.host })
+        res.json({ ip: service.host });
       });
-      app.listen(8082);
+      app.get('/doreset', function(req, res) {
+        console.log("calling reset.");
+        exec('./restart.sh', function(a, b, c) {
+          exec('./restart.sh', function(a, b, c) {
+            res.json({ok: true});
+          });
+        });
+      });
+      app.listen(8088);
     }
   });
 
   app.get('/', function(req, res) {
     app.use(express.static('./image'));
     res.sendFile(path.join(__dirname + '/index.html'));
-});
+  });
+
+  app.get('/isiceboxdown', function(req, res) {
+    app.use(express.static('./image'));
+    res.sendFile(path.join(__dirname + '/reset.html'));
+  });
